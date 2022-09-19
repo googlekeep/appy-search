a21f56fb0ca76a85dfb00acae9b766ed0d4bb056
googlekeep <info.workingforyou@gmail.com>
2022-09-19 23:14:40 +0530

    temp

---
+++ math.js
@@ -0,0 +1,12 @@
+const result = Math.pow(3, 8);
+console.log(result);
+
+const num1 = 25;
+const num2 =45;
+const gap = num1 -num2;
+if(gap < 5){
+    console.log('you guys can be friends');
+}
+else{
+    
+}
