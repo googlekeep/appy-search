96167f4daf2096d237586bfff1dcf4ea81dd8a5f
googlekeep <info.workingforyou@gmail.com>
2022-09-19 22:42:40 +0530

    split

---
+++ split-join.js
@@ -0,0 +1,17 @@
+// const lyrics = 'Tumi bondhu kala Pakhi ami jeno ki. bosonto kale tomai bolte pari ni. '
+// const parts = lyrics.split(' ');
+// console.log(parts);
+
+const lyrics = 'Tumi bondhu kala Pakhi ami jeno ki. bosonto kale tomai bolte pari ni. '
+const parts = lyrics.split(' ');
+const sentences = lyrics.split('.');
+const chars = lyrics.split('');
+// console.log(chars);
+
+
+const partical = lyrics.slice('5,8');
+// console.log(partical);
+
+
+const partical2 = lyrics.substring('5,8');
+console.log(partical2);
